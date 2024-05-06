fe29c13cb4938f3dc0f6f8e14ca1c372523726d9
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-05-06 19:14:51 +0300

    basic styles for AppBar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,31 +2,40 @@ import { Suspense } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import UserMenu from "./UserMenu";
 import { useAuth } from "../hooks/useAuth";
+import { Container } from "@mui/system";
+import { AppBar, Grid } from "@mui/material";
 
 const Layout = () => {
-  const {isLoggedIn} = useAuth()
+  const { isLoggedIn } = useAuth();
 
   return (
-    <header>
-      <nav style={{display: 'flex'}}>
-        <NavLink to="/">Home</NavLink>
-        {isLoggedIn && (
-          <div style={{display: 'flex', flexDirection: 'row'}}>
-            <NavLink to="/contacts">Contacts</NavLink>
-            <UserMenu />
-          </div>
-        )}
-        {!isLoggedIn && (
-          <>
-            <NavLink to="/logIn">Log In</NavLink>
-            <NavLink to="/register">Sign In</NavLink>
-          </>
-        )}
-      </nav>
+    <Container>
+      <AppBar>
+        <Grid
+          component="nav"
+          container
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <NavLink to="/">Home</NavLink>
+          {isLoggedIn && (
+            <>
+              <NavLink to="/contacts">Contacts</NavLink>
+              <UserMenu />
+            </>
+          )}
+          {!isLoggedIn && (
+            <div>
+              <NavLink to="/logIn">Log In</NavLink>
+              <NavLink to="/register">Sign In</NavLink>
+            </div>
+          )}
+        </Grid>
+      </AppBar>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </header>
+    </Container>
   );
 };
 
